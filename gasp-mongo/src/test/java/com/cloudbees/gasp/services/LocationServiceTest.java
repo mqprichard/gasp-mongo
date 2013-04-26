ad9e0d13813358e1513c46dc8f12192f255a9fb7
mqprichard <mqprichard@gmail>
2013-04-26 14:28:04 -0700

    Completed refactoring: added back all tests

--- gasp-mongo/src/test/java/com/cloudbees/gasp/services/LocationServiceTest.java
+++ gasp-mongo/src/test/java/com/cloudbees/gasp/services/LocationServiceTest.java
@@ -4,55 +4,27 @@ import static org.junit.Assert.*;
 
 import javax.ws.rs.core.Response;
 
-import org.codehaus.jettison.json.JSONException;
-import org.codehaus.jettison.json.JSONObject;
 import org.junit.Test;
 
-import com.cloudbees.gasp.model.Location;
 import com.cloudbees.gasp.model.LocationQuery;
-import com.google.gson.Gson;
 
 public class LocationServiceTest {
 	private final String testName = "Home";
 	private final String testAddress = "1285 Altschul Ave, Menlo Park CA";
-	private final String testFormattedAddress = "1285 Altschul Avenue, Menlo Park, CA 94025, USA";
-	private final String testLat = "37.431523";
-	private final String testLng = "-122.206428";
-	
-	// Utility method to validate and output Response body as JSONObject
-	private static JSONObject getResponseJson( Response response ) 
-			throws JSONException {
-		String jsonResponse = response.getEntity().toString();
-		assertFalse( jsonResponse.isEmpty() );
-		
-		String strJson = "{response:" + jsonResponse + "}";
-		JSONObject jObject = new JSONObject(strJson);
-		JSONObject json = jObject.getJSONObject("response");
-		assertFalse( json == null );
-		
-		return ( json );
-	}
+	private final String testGeoLocation = "{\"name\":\"Home\",\"formattedAddress\":\"1285 Altschul Avenue, Menlo Park, CA 94025, USA\",\"location\":{\"lat\":37.431523,\"lng\":-122.206428}}";
+	private final String testLatLng = "LatLng{lat=37.4315230, lng=-122.2064280}";
+	private final String testGeocoderResult = "GeocoderResult{types=[street_address], formattedAddress='1285 Altschul Avenue, Menlo Park, CA 94025, USA', addressComponents=[GeocoderAddressComponent{longName='1285', shortName='1285', types=[street_number]}, GeocoderAddressComponent{longName='Altschul Avenue', shortName='Altschul Ave', types=[route]}, GeocoderAddressComponent{longName='Sharon Heights', shortName='Sharon Heights', types=[neighborhood, political]}, GeocoderAddressComponent{longName='Menlo Park', shortName='Menlo Park', types=[locality, political]}, GeocoderAddressComponent{longName='San Mateo', shortName='San Mateo', types=[administrative_area_level_2, political]}, GeocoderAddressComponent{longName='California', shortName='CA', types=[administrative_area_level_1, political]}, GeocoderAddressComponent{longName='United States', shortName='US', types=[country, political]}, GeocoderAddressComponent{longName='94025', shortName='94025', types=[postal_code]}], geometry=GeocoderGeometry{location=LatLng{lat=37.4315230, lng=-122.2064280}, locationType=ROOFTOP, viewport=LatLngBounds{southwest=LatLng{lat=37.43017401970850, lng=-122.2077769802915}, northeast=LatLng{lat=37.43287198029149, lng=-122.2050790197085}}, bounds=null}, partialMatch=false}";
 
-	@Test
+	@Test 
 	public void uniqueResultTest() {
-		
-		LocationService locationService = new LocationService();
-		LocationQuery location = new LocationQuery(testName, testAddress);
-		Response response = locationService.checkLocation(location);
-		
 		try {
-			// Validate HTTP Return Code
+			LocationService locationService = new LocationService();
+			LocationQuery location = new LocationQuery(testName, testAddress);
+			Response response = locationService.checkLocation(location);
+		
+			// Validate HTTP Return Code and Body
 			assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
-			
-			// Validate formatted address
-			JSONObject json = getResponseJson( response );
-			assertEquals(json.get("formattedAddress"), testFormattedAddress);
-			
-			// Validate location
-			JSONObject geometry = json.getJSONObject("geometry");
-			Location myLoc = new Gson().fromJson(geometry.get("location").toString(), Location.class);
-			assertEquals(String.valueOf(myLoc.getLat()),testLat);
-			assertEquals(String.valueOf(myLoc.getLng()), testLng);
+			assertEquals(response.getEntity().toString(), testGeocoderResult );
 		}
 		catch (Exception e) {
 			fail();
@@ -60,6 +32,7 @@ public class LocationServiceTest {
 	}
 	
 	@Test
+	// tests /lookup where more than one match
 	public void ambiguousResultTest() {
 		// Ambiguous input: Geocoder will return OK with multiple Results
 		// Expect: service returns 204 No Content
@@ -99,20 +72,14 @@ public class LocationServiceTest {
 	
 	@Test
 	public void latLngTest() {
-
-		LocationService locationService = new LocationService();
-		LocationQuery location = new LocationQuery(testName, testAddress);
-		Response response = locationService.getLatLng(location);
-		
 		try {
-			// Validate HTTP Return Code
+			LocationService locationService = new LocationService();
+			LocationQuery location = new LocationQuery(testName, testAddress);
+			Response response = locationService.getLatLng(location);
+		
+			// Validate HTTP Return Code and Body
 			assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
-			
-			// Validate formatted address
-			JSONObject json = getResponseJson( response );
-			Location myLoc = new Gson().fromJson(json.toString(), Location.class);
-			assertEquals(String.valueOf(myLoc.getLat()),testLat);
-			assertEquals(String.valueOf(myLoc.getLng()), testLng);
+			assertEquals(response.getEntity().toString(), testLatLng );
 		}
 		catch (Exception e) {
 			fail();
@@ -121,23 +88,14 @@ public class LocationServiceTest {
 	
 	@Test
 	public void addLocationTest() {
-
-		LocationService locationService = new LocationService();
-		LocationQuery location = new LocationQuery(testName, testAddress);
-		Response response = locationService.addLocation(location);
-		
 		try {
-			// Validate HTTP Return Code
+			LocationService locationService = new LocationService();
+			LocationQuery location = new LocationQuery(testName, testAddress);
+			Response response = locationService.addLocation(location);
+		
+			// Validate HTTP Return Code and Body
 			assertEquals(response.getStatus(), Response.Status.OK.getStatusCode());
-			
-			// Validate formatted address
-			JSONObject json = getResponseJson( response );
-			assertEquals(json.get("formattedAddress"), testFormattedAddress);
-			
-			// Validate location 
-			Location myLoc = new Gson().fromJson(json.get("location").toString(), Location.class);
-			assertEquals(String.valueOf(myLoc.getLat()),testLat);
-			assertEquals(String.valueOf(myLoc.getLng()), testLng);
+			assertEquals(response.getEntity().toString(), testGeoLocation );
 		}
 		catch (Exception e) {
 			fail();
