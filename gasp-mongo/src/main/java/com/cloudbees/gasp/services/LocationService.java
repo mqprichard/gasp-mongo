161fb1b8614e4bb6976d62fd4b8eff235b8fe45c
mqprichard <mqprichard@gmail>
2013-04-26 13:30:50 -0700

    Refactored LocationService

--- gasp-mongo/src/main/java/com/cloudbees/gasp/services/LocationService.java
+++ gasp-mongo/src/main/java/com/cloudbees/gasp/services/LocationService.java
@@ -1,7 +1,5 @@
 package com.cloudbees.gasp.services;
 
-import java.util.Iterator;
-
 import javax.servlet.http.HttpServlet;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
@@ -21,12 +19,6 @@ import com.cloudbees.gasp.model.LocationQuery;
 import com.cloudbees.gasp.model.MongoConnection;
 import com.cloudbees.gasp.model.SpatialQuery;
 
-import com.google.code.geocoder.Geocoder;
-import com.google.code.geocoder.GeocoderRequestBuilder;
-import com.google.code.geocoder.model.GeocodeResponse;
-import com.google.code.geocoder.model.GeocoderAddressComponent;
-import com.google.code.geocoder.model.GeocoderRequest;
-import com.google.code.geocoder.model.GeocoderStatus;
 import com.google.gson.Gson;
 
 @Path("/locations")
@@ -36,44 +28,6 @@ public class LocationService extends HttpServlet {
 	private final Logger logger = LoggerFactory.getLogger(LocationService.class);
 	private MongoConnection mongoConnection = new MongoConnection();
 
-	private GeocodeResponse geocodeService( String queryAddress ) {
-		final Geocoder geocoder = new Geocoder();
-		GeocoderRequest geocoderRequest = new GeocoderRequestBuilder()
-												.setAddress(queryAddress)
-												.setLanguage("en")
-												.getGeocoderRequest();
-		GeocodeResponse geocoderResponse = geocoder.geocode(geocoderRequest);
-		
-		if  ( (geocoderResponse.getStatus() == GeocoderStatus.OK)
-			  && (geocoderResponse.getResults().size() == 1) ) {
-			logger.info("Match found for address: " + queryAddress);
-			logger.info("formattedAddress = " + geocoderResponse
-												.getResults().get(0)
-												.getFormattedAddress());
-			logger.info("Location = " + geocoderResponse
-										.getResults().get(0)
-										.getGeometry()
-										.getLocation());
-							
-			if (logger.isDebugEnabled()) {
-				Iterator<GeocoderAddressComponent> iterator = geocoderResponse
-																.getResults()
-																.get(0)
-																.getAddressComponents()
-																.iterator();
-				while (iterator.hasNext()) {
-					GeocoderAddressComponent addressComponent = iterator.next();
-					logger.debug("Address Component ["
-								 + addressComponent.getTypes().get(0)
-								 + "] = "
-								 + addressComponent.getShortName() );
-					}
-			}
-		}
-		
-		return geocoderResponse;
-	}
-	
 	@POST
     @Path("/new")
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -108,7 +62,7 @@ public class LocationService extends HttpServlet {
 				mongoConnection.connect();
 				mongoConnection.newLocation(gaspLocation);
 			
-				//We have a match: return 200 OK plus GaspLocation data
+				//Return 200 OK plus GaspLocation data
 				return Response
 						.status(Response.Status.OK)
 						.entity(new Gson().toJson(gaspLocation))
@@ -123,78 +77,6 @@ public class LocationService extends HttpServlet {
 	    	return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();		
 		}
 	}
-	
-/*
-	@POST
-    @Path("/new")
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response addLocation( LocationQuery location ) {
-		try {
-			logger.debug("Name = " + location.getName());
-			logger.debug("AddressString = " + location.getAddressString());
-			
-			GeocodeResponse geocoderResponse = geocodeService(location.getAddressString());
-
-			switch(geocoderResponse.getStatus()) {
-				
-				case ZERO_RESULTS:	// No match found: return 204 No Content
-									logger.info("No matches found for address: " + location.getAddressString());
-							   	    return Response.status(Response.Status.NO_CONTENT).build();
-			
-				case OK: 	// More than one match found: return 204 No Content
-							if (geocoderResponse.getResults().size() > 1) {
-								logger.info("Ambiguous: more than one match for address: " 
-											+ location.getAddressString());
-								return Response.status(Response.Status.NO_CONTENT).build();				
-							}
-
-							// Create a Location object from GeocoderResponse
-							Gson gson = new Gson();
-							String json = gson.toJson(geocoderResponse
-															.getResults().get(0)
-															.getGeometry()
-															.getLocation());
-							Location theLocation = gson.fromJson(json, Location.class);
-							
-							// Get formatted address string from GeocoderResponse
-							String formattedAddress = geocoderResponse.getResults().get(0).getFormattedAddress();
-							
-							// GaspLocation is stored in Mongo and returned to the client
-							GeoLocation gaspLocation = new GeoLocation(location.getName(),
-													 					 formattedAddress,
-													 					 theLocation);
-							mongoConnection.connect();
-							mongoConnection.newLocation(gaspLocation);
-							
-							//We have a match: return 200 OK plus GaspLocation data
-							return Response
-									.status(Response.Status.OK)
-									.entity(new Gson().toJson(gaspLocation))
-									.build();
-					
-					// There was a problem with the Google Geocoder Service
-					// Return 500 Internal Server Error
-					case ERROR:
-					case INVALID_REQUEST:
-					case OVER_QUERY_LIMIT:
-					case REQUEST_DENIED:
-					case UNKNOWN_ERROR: 
-					default:			
-						logger.info("Google Geocoding API returned" + geocoderResponse.getStatus());
-						return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();	
-				}
-		}
-		catch (Exception e){
-			logger.error("Exception processing location request", e);
-			// Return 500 Internal Server Error
-    		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();		
-		}
-		finally {
-			mongoConnection.getMongo().close();
-		}
-	}
-*/
 	
 	@POST
     @Path("/lookup")
